349d519089d3e34806e3368edd26ef1908448662
Yvette334 <y.muhoracye@alustudent.com>
2025-10-22 19:43:46 +0200

    added

--- mmovie-explorer/src/components/CategoryFilter.jsx
+++ mmovie-explorer/src/components/CategoryFilter.jsx
@@ -3,7 +3,7 @@ export default function CategoryFilter({ genres, selected, onChange }) {
     <select
       value={selected}
       onChange={(e) => onChange(e.target.value)}
-      className=""
+      className="border rounded-lg py-4 px-4 bg-slate-800 text-white"
     >
       <option value="">All Categories</option>
       {genres.map((g) => (
