6588f9f8869e0f6cf057845b9c93816f06ca669c
Yvette334 <y.muhoracye@alustudent.com>
2025-10-22 19:28:18 +0200

    added

--- mmovie-explorer/src/hooks/useFetchMovies.js
+++ mmovie-explorer/src/hooks/useFetchMovies.js
@@ -2,25 +2,22 @@ import { useEffect, useState } from "react";
 import { getAllMovies } from "../utils/api";
 
 export default function useFetchMovies() {
+  // 🧩 make small boxes to store info
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
 
-  useEffect(() => {
-    async function fetchMovies() {
-      try {
-        const data = await getAllMovies();
-        console.log("Movies fetched:", data); 
-        setMovies(data);
-      } catch (err) {
-        console.log("Fetch error:", err);
-        setError("Failed to load movies");
-      } finally {
-        setLoading(false);
-      }
-    }
-    fetchMovies();
-  }, []);
 
+  useEffect(() => {
+    getAllMovies()
+      .then((data) => {
+        setMovies(data);
+        setLoading(false);
+      })
+      .catch(() => {
+        setError("Could not load movies.");
+        setLoading(false);
+      });
+  }, []);
   return { movies, loading, error };
 }
